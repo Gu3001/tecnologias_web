723512147ce018c8dd34cd7485cc1da69835e5e1
gustavo <gustavo180g@gmail.com>
2018-11-26 10:37:02 -0200

    terminado trabalho freelancer

---
+++ 3_trimestre/teste/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+  </head>
+  <body>
+    <h2>Trabalhos:</h2>
+
+
+      <ul>
+        <li> <a href="http://luizifpr.epizy.com/trabalhoPlugin/">Trabalho Plugin Jquery</a></li>
+        <li> <a href="http://luizifpr.epizy.com/trabalhoFreelancer/">Trabalho site Freelancer</a></li>
+
+      </ul>
+    </p>
+
+<?php
+
+ ?>
